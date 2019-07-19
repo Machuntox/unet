604746998472e1c8cea1b14ee654f54067e0d51b
Stefan berger <stefanbergerleo@yahoo.de>
2019-07-19 18:51:22 +0200

    import missing dependencies

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import os
 import glob
 import skimage.io as io
 import skimage.transform as trans
-from skimage import img_as_ubyte
+from skimage import img_as_ubyte, img_as_float
 
 Sky = [128,128,128]
 Building = [128,0,0]
