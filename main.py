31ae8d4883667ab3460fe84b1ac3b8ec6bdd3cb7
Stefan berger <stefanbergerleo@yahoo.de>
2019-07-22 17:24:47 +0200

    small unet and pictures

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,'data/JOSM/train','image','label',data_gen_args,save_to_dir = None)
 
-model = unet()
+model = unetNorm()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=300,epochs=20,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+testGene = testGenerator("data/JOSM/test")
+model = unetNorm()
+model.load_weights("unet_membrane.hdf5")
+results = model.predict_generator(testGene,1,verbose=1)
+saveResult("data/JOSM/result",results)
---
+++ prepareData.py
@@ -0,0 +1,51 @@
+# greyscale img
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import PIL.ImageOps
+from skimage import util
+import os
+
+def cropImages():
+    imageFolder = 'data/JOSM/train/imageRaw'
+    labelFolder = 'data/JOSM/train/labelRaw'
+
+
+    for image_name in os.listdir(imageFolder):
+        image_obj = Image.open(imageFolder + '/' + image_name)
+        cropped_image = image_obj.crop((0, 0, 512, 512)) #(x1, y1, x2,y2)
+        cropped_image.save('data/JOSM/train/imageCropped/' + image_name)
+        
+    for image_name in os.listdir(labelFolder):
+        image_obj = Image.open(labelFolder + '/' + image_name)
+        cropped_image = image_obj.crop((0, 0, 512, 512)) #(x1, y1, x2,y2)
+        cropped_image.save('data/JOSM/train/labelCropped/' + image_name)
+
+def preprocessImage():
+    folder = 'data/JOSM/train/imageCropped'
+
+    for image_name in os.listdir(folder):
+        img = Image.open(folder + '/' + image_name).convert('LA')
+        img.save('data/JOSM/train/image/' + image_name)
+
+
+# see here: https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
+
+
+
+def preprocessLayer():
+    folder = 'data/JOSM/train/labelCropped'
+
+    for image_path in os.listdir(folder):
+        img = Image.open(folder + '/' + image_path)
+        inverted_img_pixels = np.ndarray(shape=(img.size[0], img.size[1]), dtype=float)
+        for x in range(0, img.size[0]):
+            for y in range(0, img.size[1]):
+                pixel = img.getpixel((x, y))
+                inverted_img_pixels[x][y] = 255 if pixel == (0,0,0) else 0
+        plt.imsave('data/JOSM/train/label/' + image_path, inverted_img_pixels, cmap=cm.gray)
+
+cropImages = cropImages()
+preprocessImage = preprocessImage()
+preprocessLayer = preprocessLayer()
