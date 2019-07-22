83d4b2429ee5c51be85a48101ea2022267f09048
Stefan berger <stefanbergerleo@yahoo.de>
2019-07-22 17:27:33 +0200

    bugfix

--- model.py
+++ model.py
@@ -66,6 +66,10 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
 def unetSmall(pretrained_weights = None,input_size = (256,256,1)):
     inputs = Input(input_size)
 
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    
     conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
     conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
@@ -80,7 +84,7 @@ def unetSmall(pretrained_weights = None,input_size = (256,256,1)):
     conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
 
     up5 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv4))
-    merge5 = concatenate([conv1,up5],axis=3
+    merge5 = concatenate([conv1,up5],axis=3)
     conv5 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge5)
     conv5 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
     conv5 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
