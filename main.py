645331b917434e1588701aeaa3079225097386b3
Stefan berger <stefanbergerleo@yahoo.de>
2019-07-27 23:54:17 +0200

    new try multi gpu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from model import *
 from data import *
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 
 data_gen_args = dict(rotation_range=0.2,
@@ -13,12 +13,12 @@ data_gen_args = dict(rotation_range=0.2,
                     fill_mode='nearest')
 myGene = trainGenerator(2,'data/JOSM/train','image','label',data_gen_args,save_to_dir = None)
 
-model = unet()
+model = unetNorm()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=13,epochs=1000,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=300,epochs=20,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/JOSM/test")
-model = unet()
+model = unetNorm()
 model.load_weights("unet_membrane.hdf5")
 results = model.predict_generator(testGene,1,verbose=1)
 saveResult("data/JOSM/result",results)
--- model.py
+++ model.py
@@ -8,7 +8,6 @@ from keras.layers import *
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
-from keras.utils import multi_gpu_model
 
 
 
@@ -55,9 +54,8 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(inputs = inputs, outputs = conv10)
-    parallel_model = multi_gpu_model(model, gpus=2, cpu_merge=True, cpu_relocation=False)
 
-    parallel_model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
@@ -95,9 +93,8 @@ def unetSmall(pretrained_weights = None,input_size = (256,256,1)):
     conv6 = Conv2D(1, 1, activation = 'sigmoid')(conv5)
 
     model = Model(inputs = inputs, outputs = conv6)
-    parallel_model = multi_gpu_model(model, gpus=2, cpu_merge=True, cpu_relocation=False)
 
-    parallel_model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
@@ -173,9 +170,8 @@ def unetNorm(pretrained_weights = None,input_size = (256,256,1)):
     #conv10 = BatchNormalization()(conv10)
 
     model = Model(inputs = inputs, outputs = conv10)
-    parallel_model = multi_gpu_model(model, gpus=2, cpu_merge=True, cpu_relocation=False)
 
-    parallel_model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
