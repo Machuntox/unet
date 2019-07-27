e297f85c4324c5eea1b2cbe361697fd0f5ee3dd8
Stefan berger <stefanbergerleo@yahoo.de>
2019-07-27 10:20:21 +0200

    grey image bugfix

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import os
 import glob
 import skimage.io as io
 import skimage.transform as trans
-from skimage import img_as_float
+from skimage import img_as_uint
 
 Sky = [128,128,128]
 Building = [128,0,0]
@@ -86,7 +86,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
     for i in range(num_image):
         img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
-        img = img / 255.
+        img = img / 255. 
         img = trans.resize(img,target_size)
         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
         img = np.reshape(img,(1,)+img.shape)
@@ -122,4 +122,4 @@ def labelVisualize(num_class,color_dict,img):
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"%d_predict.jpg"%i),img_as_float(img))
+        io.imsave(os.path.join(save_path,"%d_predict.tif"%(i)),img_as_uint(img))
