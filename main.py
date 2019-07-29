96fde8fe07e1eca931fb27c5b6295dc01ff23ad1
Machuntox <mrmachun@gmail.com>
2019-07-29 07:49:37 +0200

    change knn to unetNorm

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ data_gen_args = dict(rotation_range=0.2,
                     fill_mode='nearest')
 myGene = trainGenerator(2,'data/JOSM/train','image','label',data_gen_args,save_to_dir = None)
 
-model = unet()
+model = unetNorm()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=50,epochs=1000,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=50,epochs=3000,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/JOSM/test")
-model = unet()
+model = unetNorm()
 model.load_weights("unet_membrane.hdf5")
 results = model.predict_generator(testGene,20,verbose=1)
 saveResult("data/JOSM/result",results)
